8df2250ef6f7f716b29c326935ec96e02a73a18d
Zao01 <adm.marcosfonseca@gmail.com>
2025-04-08 12:47:51 -0300

    atualizaçao do arquivo csv em excel e alteração no codigo

--- codigo 1.py
+++ codigo 1.py
@@ -1,4 +1,4 @@
 import pandas as pd
-df = pd.read_excel("C:\Users\User\Desktop\PROJETOS\Projeto-PyPandas\Desempenho_Equipe.csv")
+df = pd.read_excel("C:/Users/User/Desktop/PROJETOS/Projeto-PyPandas/Desempenho_Equipe1.xlsx")
 print(df.head())
 
